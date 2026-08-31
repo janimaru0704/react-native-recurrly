63a491a675c2a68b59b142522f6d0d37e09fd0ce
janimaru0704 <sod0704.225@gmail.com>
2026-08-31 10:07:42 +0900

    プロジェクト用のアセットを追加

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -11,7 +11,7 @@ const Index = () => {
             <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign in</Link>
             <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign up</Link>
 
-            <Link href="/subscriptions/spotify">Spotify Subscription</Link>
+            <Link href="/src/app/subscriptions/spotify">Spotify Subscription</Link>
             <Link href={{
                 pathname: "/subscriptions/[id]",
                 params: {id: "claude"},
