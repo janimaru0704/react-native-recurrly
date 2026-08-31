79a68c6efe1965f75a190624bef1ed53fef2d86e
janimaru0704 <sod0704.225@gmail.com>
2026-08-31 09:48:42 +0900

    Expo Routerのテスト用に画面を追加

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,7 +1,6 @@
 import {Stack} from "expo-router";
-import "@/global.css";
 
-const RootLayout = () => {
+const AuthLayout = () => {
     return <Stack screenOptions={{headerShown: false}}/>;
 }
-export default RootLayout;
+export default AuthLayout;
---
+++ src/app/(tabs)/subscriptions.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+
+const Subscriptions = () => {
+    return (
+        <View>
+            <Text>Subscriptions</Text>
+        </View>
+    );
+};
+export default Subscriptions;
