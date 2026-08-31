89ed65425633f343b2a8fbdf8c13a7aa13d97846
janimaru0704 <sod0704.225@gmail.com>
2026-08-31 09:24:06 +0900

    Expo Routerのテスト用に画面を追加

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View } from "react-native";
+import {Link} from "expo-router";
 
 const Index = () => {
     return (
@@ -6,6 +7,7 @@ const Index = () => {
             <Text className="text-xl font-bold text-success">
                 Welcome to Nativewind!
             </Text>
+            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4" >Go to Onboarding</Link>
         </View>
     );
 };
